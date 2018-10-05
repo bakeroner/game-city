59a785bed27947782c717f56ab0db681d4db1e28
AM_PC <ashakhtorin@inbox.ru>
2018-10-05 20:30:28 +0300

    checking letters is added to script

--- js/game.js
+++ js/game.js
@@ -7,13 +7,14 @@ class cityWord {
 		this.word = cityNameField.value;
 		this.score = 0;
 		this.dialogArray = [];
+		this.answerArray = ["Питер","Минск","Пинск","Краснодар","Кадужный"];
 		this.lastLetter = "";
+		this.correctLastLetter = "";
 	}
 
-	check () {
-		console.log(this.word);
+	validation () {
 		if (this.word != "") {
-			return true;
+				return true;
 		}
 		else {
 			return false;
@@ -31,30 +32,54 @@ class cityWord {
 		dialogLog.appendChild(item);
 	}
 
-	lastLetter () {
-		/*take last letter*/
+	uesrLastLetter () {
+		let wordLength = this.word.length;
+		let letter = this.word.charAt(wordLength - 1);
+		this.lastLetter = letter;
+		return letter;
 	}
 
-	score () {
-		/*score count*/
+	endOfLastWord () {
+		let lastWord = this.dialogArray[this.dialogArray.length-1];
+		let wordLength = lastWord.length;
+		let letter = lastWord.charAt(wordLength - 1);
+		this.correctLastLetter = letter;
+		return letter;
+	}
+
+	scoreAdd () {
+		this.score++;
 	}
 
 	answer () {
-		/*console.log("123");*/
-		this.dialogArray.push("123");	
-		let item = document.createElement("p");
-		item.innerHTML = this.dialogArray[this.dialogArray.length-1];;
-		dialogLog.appendChild(item);	
+		let answerLength = this.answerArray.length;
+		let letter = this.correctLastLetter.toUpperCase();
+		for (let i=0; i<answerLength; i++) {
+			if (this.answerArray[i].charAt(0) == letter) {
+		this.dialogArray.push(this.answerArray[i]);
+		break;
+		}
+		}
 	}
 }
+
 submitButton.addEventListener("click", function (event) {
 	event.preventDefault();
 	let p = new cityWord();
-	if (p.check() == true) {
-		p.log();
-		p.wordOutput();
-		p.answer();
+	if (p.validation() == true) {
+		if (p.correctLastLetter == "" || p.userLastLetter() == p.endOfLastWord()) {
+			p.log();
+			p.endOfLastWord();
+			p.wordOutput();
+			p.scoreAdd();
+			p.answer();
+			p.endOfLastWord();
+			p.wordOutput();
+			console.log(p.score);
+			}
+			else {
+				console.log("Wrong input");
+		}
 	}
-
 })
 
