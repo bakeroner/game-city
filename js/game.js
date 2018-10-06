48aaf3722221dbd707594825bf99cc9246d5b899
AM_PC <ashakhtorin@inbox.ru>
2018-10-06 03:25:04 +0300

    check for repeating added

--- js/game.js
+++ js/game.js
@@ -7,7 +7,8 @@ class cityWord {
 		this.word = cityNameField.value;
 		this.score = 0;
 		this.dialogArray = [];
-		this.answerArray = ["Питер","Минск","Пинск","Краснодар","Радужный"];
+		this.usedArray = [];
+		this.answerArray = ["Питер", "Минск", "Пинск", "Краснодар", "Радужный", "Адана"];
 		this.lastLetter = "";
 		this.correctLastLetter = "";
 	}
@@ -20,7 +21,7 @@ class cityWord {
 			for (let i = 1; i<this.word.length; i++) {
 				userLetters = this.word.charAt(i).toLowerCase();
 				userWord += userLetters;
-			}			
+			}
 			this.word = userWordFirstLetter+userWord;
 				return true;
 		}
@@ -34,14 +35,20 @@ class cityWord {
 	}
 
 	wordOutput () {
+		let lastElement = this.dialogArray[this.dialogArray.length-1];
 		let item = document.createElement("p");
-		item.innerHTML = this.dialogArray[this.dialogArray.length-1];
+		this.usedArray.push(lastElement);
+		item.innerHTML = lastElement;
 		dialogLog.appendChild(item);
 	}
 
 	userLastLetter () {
-		let wordLength = this.word.length;
-		let letter = this.word.charAt(wordLength - 1);
+		let userWord = this.word;
+		let wordLength = userWord.length;
+		let letter = userWord.charAt(wordLength - 1);
+		if (letter == "ъ" || letter == "ь") {
+			letter = userWord.charAt(wordLength - 2);
+		}
 		this.lastLetter = letter;
 		return letter;
 	}
@@ -50,6 +57,9 @@ class cityWord {
 		let lastWord = this.dialogArray[this.dialogArray.length-1];
 		let wordLength = lastWord.length;
 		let letter = lastWord.charAt(wordLength - 1);
+		if (letter == "ъ" || letter == "ь") {
+			letter = lastWord.charAt(wordLength - 2);
+		}
 		this.correctLastLetter = letter;
 		return letter;
 	}
@@ -61,12 +71,27 @@ class cityWord {
 
 	answer () {
 		let answerLength = this.answerArray.length;
+		let usedLength = this.usedArray.length;
 		let letter = this.correctLastLetter.toUpperCase();
+		let check = true;
 		for (let i = 0; i<answerLength; i++) {
 			if (this.answerArray[i].charAt(0) == letter) {
-		this.dialogArray.push(this.answerArray[i]);
-		break;
-		}
+				for (let j = 0; j<usedLength; j++) {
+					if (this.usedArray[j] == this.answerArray[i])
+					{
+						i++;
+						check = false;
+						break;
+					}
+					else {
+						check = true;
+					}
+				}
+				if (check) {
+					this.dialogArray.push(this.answerArray[i]);
+					break;
+				}
+			}
 			else {
 				this.dialogArray.push("You won");
 			}
@@ -88,8 +113,14 @@ submitButton.addEventListener("click", function (event) {
 			p.wordOutput();
 			}
 			else {
-				console.log("Wrong input");
+				p.dialogArray.push("Word have to start from the previous word's last letter");
+				p.wordOutput();
 		}
 	}
+	else {
+			p.dialogArray.push("Wrong input");
+			p.wordOutput();
+		}
+	
 })
 
